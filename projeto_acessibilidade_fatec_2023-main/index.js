010b7947518cc6311fb63cf2b3c5aa46d0f3fe62
mmsousaa <146947346+mmsousaa@users.noreply.github.com>
2023-12-11 07:38:48 -0300

    Update index.js

--- projeto_acessibilidade_fatec_2023-main/index.js
+++ projeto_acessibilidade_fatec_2023-main/index.js
@@ -6,3 +6,23 @@ button.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
 document.body.appendChild(button);
+}
+
+let zoomLevel = 1;
+
+function zoomIn() {
+  zoomLevel += 0.1;
+  applyZoom();
+}
+
+function zoomOut() {
+  if (zoomLevel > 1) {
+    zoomLevel -= 0.1;
+    applyZoom();
+  }
+}
+
+function applyZoom() {
+  const zoomTarget = document.getElementById('zoom-target');
+  zoomTarget.style.transform = scale(${zoomLevel});
+}
